736afa320bf60e65cfd982adb4f3190cd144d76f
Skumar101 <skumar1@stu.naperville203.org>
2022-11-18 11:26:25 -0600

    commit backstreet boys

--- TreeIntro/TreeDemoSimple.java
+++ TreeIntro/TreeDemoSimple.java
@@ -9,10 +9,13 @@ public class TreeDemoSimple
 {
     public static void main()
     {
+        BinaryTree t5 = new BinaryTree("Charlie");
         BinaryTree t4 = new BinaryTree("Savannah");
         BinaryTree t2 = new BinaryTree("Peter", t4,null);
-        BinaryTree t3 = new BinaryTree("Zara");
+        BinaryTree t3 = new BinaryTree("Zara",null,t5);
+        //BinaryTree t3 = new BinaryTree("Zara");
         BinaryTree t1 = new BinaryTree("Anne",t2,t3);
+        System.out.println("Expected: 2");
         System.out.println("Size: "+t1.countNodesWithOneChild());
 
     }
